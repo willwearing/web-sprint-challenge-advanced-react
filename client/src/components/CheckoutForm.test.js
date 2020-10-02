85f652f948e6c5cc21f165a1c816f75d13f0a20e
willwearing <willwearing@gmail.com>
2020-10-02 10:10:34 -0600

    expect header test is complete and working

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -6,6 +6,8 @@ import CheckoutForm from "./CheckoutForm";
 
 test("form header renders", () => {
   render(<CheckoutForm />);
+  const header = screen.getByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
 });
 
 test("form shows success message on submit with form details", () => {});
